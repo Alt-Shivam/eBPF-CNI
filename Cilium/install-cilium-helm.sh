eb2e4ec085871289c7bb0a8e3cc5def3b6528289
Shivank Chaudhary <81817735+Alt-Shivam@users.noreply.github.com>
2022-08-28 13:49:19 +0530

    Create install-cilium-helm.sh

---
+++ Cilium/install-cilium-helm.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Add Cilium remote repo for helm
+helm repo add cilium https://helm.cilium.io/
+
+# Deploy Cilium in Kube-System Namespace with specified release
+helm install cilium cilium/cilium --version 1.10.14 \
+  --namespace kube-system
+
+# wait for pods to come up
+sleep 20
+
+# Cilium installation test
+curl -L --remote-name-all https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-amd64.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-amd64.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin
+rm cilium-linux-amd64.tar.gz{,.sha256sum}
+
+# Run Tests
+cilium connectivity test
+flag=${echo $?}
+
+# Test the deployment
+if [flag!=0]
+then  
+    echo "Deployment failed, there is something not right!"
+    exit 1
+else
+    echo "Cilium Deployed Successfully!"
+    exit 0
+fi
