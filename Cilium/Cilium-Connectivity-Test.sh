c3dddbdd504ffcbf8a7d211097216aa5f6d3af6b
Shivank Chaudhary <81817735+Alt-Shivam@users.noreply.github.com>
2022-09-01 11:20:41 +0530

    Update Cilium-Connectivity-Test.sh

--- Cilium/Cilium-Connectivity-Test.sh
+++ Cilium/Cilium-Connectivity-Test.sh
@@ -2,3 +2,13 @@
 
 # Cilium test for connectivity.
 cilium connectivity test
+flag=$(echo $?)
+
+# Test the deployment
+if [ flag != 0 ]; then
+    echo "Connectivity Test failed!, please reinstall cilium or check your K8s cluster!"
+    exit 1
+else
+    echo "Cilium Deployed Successfully!"
+    exit 0
+fi
